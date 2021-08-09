d788c6646a7c4cd4cf4d7b734b29f3f02f6a5c71
Subham Das <57589556+dsubham76@users.noreply.github.com>
2021-08-09 21:04:56 +0530

    Update face_capture.py

--- face_capture.py
+++ face_capture.py
@@ -1,13 +1,22 @@
 import cv2
 import uuid
+import os
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--name", required=True,help="input first name")
+args = vars(ap.parse_args())
+
+# nameClass = input('Enter your first name : ')
+os.mkdir(os.path.join('./dataset/',args["name"]))
 
 cap = cv2.VideoCapture(0)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-i = 0
+
 while True:
     ret, frame = cap.read()
-    cv2.imwrite('./Images/cam'+str(uuid.uuid4())+'.jpg',frame)
+    cv2.imwrite('./dataset/'+args["name"]+'/'+args["name"]+str(uuid.uuid4())+'.jpg',frame)
     cv2.imshow('frame',frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
